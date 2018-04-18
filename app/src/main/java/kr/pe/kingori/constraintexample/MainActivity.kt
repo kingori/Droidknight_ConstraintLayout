7b8e7de76a80421a9eb5d840b480cf9f96df5190
WINGORI-ZENBOOK\kingori <kingori@gmail.com>
2018-04-19 00:44:30 +0900

    빌드 오류 수정 ㅠㅠ

--- app/src/main/java/kr/pe/kingori/constraintexample/MainActivity.kt
+++ app/src/main/java/kr/pe/kingori/constraintexample/MainActivity.kt
@@ -5,17 +5,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.chain_weighted.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placeholder_orientation)
-
-        btn_constraintset?.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ConstraintSetTestActivity::class.java))
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
